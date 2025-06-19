66669315fbc69623aedd765f745abb507c92ea0e
Garik-Mk <garik.mkrtchyan@boo-vision.com>
2025-06-19 10:57:06 +0400

    Added gitignore, and trying something with bmp

--- SegmentetionTask.py
+++ SegmentetionTask.py
@@ -1,22 +1,26 @@
 import os
 import cv2
 
+input_folder = r'C:\Users\User\Desktop\TigranSegmentetion\DataSpecialForTigran'
+output_folder = r'C:\Users\User\Desktop\TigranSegmentetion\Processed'
 
-folder_path = r'C:\Users\User\Desktop\TigranSegmentetion\DataSpecialForTigran'
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
-for filename in os.listdir(folder_path):
+for filename in os.listdir(input_folder):
     if filename.lower().endswith('.bmp'):
-        file_path = os.path.join(folder_path, filename)
+        file_path = os.path.join(input_folder, filename)
 
         image = cv2.imread(file_path)
+        assert image is not None, f"Failed {filename}"
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
         
-        if image is None:
-            print(f"error {filename}")
-            continue
+        print(f"Processing {filename}")
 
-        cv2.imshow('Image', image)
-        print(f"Showing {filename}")
-
-        cv2.waitKey(0)
+        output_path = os.path.join(output_folder, filename)
+        cv2.imwrite(output_path, thresh)
 
 cv2.destroyAllWindows()
