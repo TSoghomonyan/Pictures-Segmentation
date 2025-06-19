3a6d4bd714fd0917ad982176bbf709ebb872e56c
Garik-Mk <garik.mkrtchyan@boo-vision.com>
2025-06-19 11:47:39 +0400

    work with openCV, try metod watershed

--- SegmentetionTask.py
+++ SegmentetionTask.py
@@ -1,8 +1,9 @@
 import os
 import cv2
+import numpy as np
 
 input_folder = r'C:\Users\User\Desktop\TigranSegmentetion\DataSpecialForTigran'
-output_folder = r'C:\Users\User\Desktop\TigranSegmentetion\Processed'
+output_folder = r'C:\Users\User\Desktop\TigranSegmentetion\Processed1'
 
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
@@ -17,10 +18,31 @@ for filename in os.listdir(input_folder):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-        
+
         print(f"Processing {filename}")
 
-        output_path = os.path.join(output_folder, filename)
-        cv2.imwrite(output_path, thresh)
+        
+        kernel = np.ones((3, 3), np.uint8)
+        opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
+
+        sure_bg = cv2.dilate(opening, kernel, iterations=3)
+
+        dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+        ret, sure_fg = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
+
+        sure_fg = np.uint8(sure_fg)
+        unknown = cv2.subtract(sure_bg, sure_fg)
+
+        # Saving results
+        name, ext = os.path.splitext(filename)
+        # output_path = os.path.join(output_folder, filename)
+        # cv2.imwrite(output_path, unknown)
+
+        # output_path = os.path.join(output_folder, filename)
+        # cv2.imwrite(output_path, thresh)
+        cv2.imwrite(os.path.join(output_folder, f"{name}_thresh.bmp"), thresh)
+        cv2.imwrite(os.path.join(output_folder, f"{name}_open.bmp"), opening)
+        cv2.imwrite(os.path.join(output_folder, f"{name}_surefg.bmp"), sure_fg)
+        cv2.imwrite(os.path.join(output_folder, f"{name}_unknown.bmp"), unknown)
 
 cv2.destroyAllWindows()
